543e9b9de594c9a125854f31daa87439f3a99d30
Chenzier Kabrabam <chenzierkabrabam@hotmail.com>
2022-01-13 19:15:29 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import express, { Express } from 'express'
+import express, { Express, Request, Response } from 'express'
 import { movieRouter } from './movies/movies'
 import { showRouters } from './tv-show/show'
 
 const app: Express = express()
-const PORT: number = 3000
+const PORT: string | number = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.get('/', (req: Request, res: Response): void => {
+  res.send('welcome to torrent api')
+})
+
 app.use('/api/v1/movies', movieRouter)
+
 app.use('/api/v1/tv_shows', showRouters)
 
 app.listen(PORT, () => console.log(`server listening at port ${PORT}`))
