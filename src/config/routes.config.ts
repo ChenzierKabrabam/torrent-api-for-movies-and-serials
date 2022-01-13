c523c06587aef282a1206369c953dff0257278f0
Chenzier Kabrabam <chenzierkabrabam@hotmail.com>
2022-01-13 18:44:08 +0530

    Delete src/config directory

--- src/config/routes.config.ts
+++
@@ -1,9 +0,0 @@
-// import { MovieRoutes } from "../@types/routes";
-
-const baseURL: string = 'https://yts.mx/api/v2/list_movies.json'
-
-export const Movie = {
-  getLatestMovie: `${baseURL}?sort_by=year`,
-  searchMovie: `${baseURL}?query_term=avenger&sort_by=year`,
-  getMovieByGenre: `${baseURL}?sort_by=year&genre=animation`,
-}
