d423c9ee9ad1a3dc78640cbdc3807c6dcbfa6313
Chenzier Kabrabam <chenzierkabrabam@hotmail.com>
2022-01-13 18:44:57 +0530

    Update routes.d.ts

--- src/@types/routes.d.ts
+++ src/@types/routes.d.ts
@@ -1,38 +1,3 @@
-// oneom api endpoint https://oneom.is/ for tv show and serial
-// All call must have Accept : application/json in all request headers, otherwise you will get html
-// - serial/{id}
-// - ep/{id}
-// - search/serial? -'title'
-// -'network_id'
-// -'country_id'
-// -'genre_id'
-// -'lang_id'
-// -'people_id'
-// -'character_id'
-// -'tvrage_id'
-// -'tvmaze_id'
-// -'mdb_id'
-// -'tvdb_id'
-// -'status_id'
-// -'runtime'
-// -'start'
-// -'end
-
-// type Title = 'title'
-// type Year = 'year'
-// type Rating = 'rating'
-// type Peers = 'peers'
-// type Seeds = 'seeds'
-// type DownloadCount = 'download_count'
-// type LikeCount = 'like_count'
-// type DateAdded = 'date_added'
-// type Descending = 'desc'
-// type Ascending = 'asc'
-// type All = 'all'
-
-// type sort_by = Title | Year | Rating | Peers | Seeds | DownloadCount | LikeCount | DateAdded
-// type order_by = Descending | Ascending
-
 export interface MovieQueryParams {
   quality: string | QueryString.ParsedQs | string[] | QueryString.ParsedQs[]
   genre: string | QueryString.ParsedQs | string[] | QueryString.ParsedQs[]
@@ -44,8 +9,4 @@ export interface MovieQueryParams {
 export interface MovieDetailQueryParams {
   withImage: string | QueryString.ParsedQs | string[] | QueryString.ParsedQs[]
   withCast: string | QueryString.ParsedQs | string[] | QueryString.ParsedQs[]
-}
-
-export interface TvShowQueryParams {
-  title: string | QueryString.ParsedQs | string[] | QueryString.ParsedQs[]
 }
